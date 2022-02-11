2c4177ccae12136cb63ffd4228d95fed318509f5
Mar Bert John Mataverde <marbertjohnmataverde@protonmail.com>
2022-02-11 15:36:06 +0800

    🚜Refactor Sign In Body - Imports

--- lib/views/signin body/signin_body.dart
+++ lib/views/signin body/signin_body.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:sizer/sizer.dart';
-import 'package:weconnect/constant/constant.dart';
-import 'package:weconnect/views/web%20view/sign%20in/web_view.dart';
 
 import '../phone view/sign in/phone_view.dart';
+import '../web view/sign in/web_view.dart';
 
 class SignInBody extends StatelessWidget {
   const SignInBody({
