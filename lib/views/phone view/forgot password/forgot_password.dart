bcb9dbb7920d2c5bff28154513eb58665d4f00ca
Mar Bert John Mataverde <marbertjohnmataverde@protonmail.com>
2022-02-11 23:34:54 +0800

    🔏Forgot Password

---
+++ lib/views/phone view/forgot password/forgot_password.dart
@@ -0,0 +1,142 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:get/get.dart';
+import 'package:sizer/sizer.dart';
+import 'package:weconnect/auth/auth.dart';
+import 'package:weconnect/constant/constant.dart';
+import 'package:weconnect/constant/constant_colors.dart';
+import 'package:weconnect/constant/constant_login_page.dart';
+
+import '../../../widgets/widget sign in/widget_custom_button.dart';
+import '../../../widgets/widget sign in/widget_textformfield_login.dart';
+
+final TextEditingController _emailCtrlr = TextEditingController();
+
+// Validation Key
+final _validationKey = GlobalKey<FormState>();
+
+final authentication = Get.put(Authentication());
+
+class ForgotPassword extends StatefulWidget {
+  const ForgotPassword({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<ForgotPassword> createState() => _PhoneViewState();
+}
+
+class _PhoneViewState extends State<ForgotPassword> {
+  bool isLoading = false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: kLoginLoginAppBarBackButton,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.only(
+            left: kPagePaddingHorizontal.w,
+            right: kPagePaddingHorizontal.w,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Forgot',
+                    style: TextStyle(
+                      color: Get.theme.primaryColor,
+                      fontWeight: FontWeight.w700,
+                      fontSize: 18.sp,
+                    ),
+                  ),
+                  Text(
+                    'Your Password 🤔',
+                    style: TextStyle(
+                      color: Get.theme.primaryColor,
+                      fontWeight: FontWeight.w700,
+                      fontSize: 18.sp,
+                      height: 0.9,
+                    ),
+                  ),
+                ],
+              ),
+              Center(
+                child: Image.asset(
+                  'assets/vectors/forgot_password.png',
+                  width: double.infinity,
+                  fit: BoxFit.cover,
+                ),
+              ),
+              Center(
+                child: Text(
+                  'Enter your email address and we will\nsend you a link to reset your\npassword! 😉',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Get.isDarkMode
+                        ? kTextColorDarkTheme
+                        : kTextColorLightTheme,
+                    fontSize: 12.sp,
+                  ),
+                ),
+              ),
+              SizedBox(height: 2.h),
+              Form(
+                key: _validationKey,
+                child: CustomTextFormField(
+                  ctrlr: _emailCtrlr,
+                  hint: 'Email Address',
+                  isPassword: kFalse,
+                  keyboardType: TextInputType.emailAddress,
+                  validator: (value) {
+                    bool _isEmailValid = RegExp(
+                            r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                        .hasMatch(value!);
+                    if (value.isEmpty) {
+                      return 'Please Enter Your Email😊';
+                    }
+                    if (!_isEmailValid) {
+                      return 'Invalid Email😐';
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              SizedBox(height: 3.h),
+              isLoading
+                  ? SpinKitSpinningLines(
+                      color: Get.theme.primaryColor,
+                      lineWidth: 1,
+                      itemCount: 5,
+                      size: 50,
+                    )
+                  : CustomButton(
+                      onPress: () async {
+                        setState(() {
+                          isLoading = true;
+                        });
+                        final _isValid =
+                            _validationKey.currentState!.validate();
+
+                        if (_isValid == true) {
+                          await authentication.resetPassword(_emailCtrlr.text);
+                        }
+                        setState(() {
+                          isLoading = false;
+                        });
+                      },
+                      text: 'Reset Now ⚡',
+                      textColor: Get.theme.primaryColor,
+                      bgColor: Get.isDarkMode
+                          ? kTextFormFieldColorDarkTheme
+                          : kTextFormFieldColorLightTheme,
+                    ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
