ba32e8bcca1fc085035c60daed3c92ec371df51d
Mar Bert John Mataverde <marbertjohnmataverde@protonmail.com>
2022-02-11 15:34:45 +0800

    🚜Refactor Phone View - Stateful 🧱

--- lib/views/phone view/sign in/phone_view.dart
+++ lib/views/phone view/sign in/phone_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:get/get.dart';
 import 'package:sizer/sizer.dart';
 import 'package:weconnect/auth/auth.dart';
@@ -7,24 +8,49 @@ import 'package:weconnect/constant/constant_colors.dart';
 
 import '../../../widgets/widget sign in/widget_custom_button.dart';
 import '../../../widgets/widget sign in/widget_textformfield_login.dart';
+import '../sign up/student sign up/student_signup_page.dart';
 
 final TextEditingController _emailCtrlr = TextEditingController();
 final TextEditingController _passwordCtrlr = TextEditingController();
 
 final authentication = Get.put(Authentication());
 
-class PhoneView extends StatelessWidget {
+class PhoneView extends StatefulWidget {
   const PhoneView({
     Key? key,
   }) : super(key: key);
 
   @override
+  State<PhoneView> createState() => _PhoneViewState();
+}
+
+class _PhoneViewState extends State<PhoneView> {
+  bool isLoading = false;
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+        actions: [
+          TextButton(
+            onPressed: () {
+              Get.to(() => const StudentSignUpPage());
+            },
+            child: Text(
+              'Sign Up',
+              style: TextStyle(
+                fontSize: 12.sp,
+                color: Get.theme.primaryColor,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ],
+      ),
       body: SingleChildScrollView(
         child: Padding(
           padding: EdgeInsets.only(
-            top: kPagePaddingVertial.h,
             left: kPagePaddingHorizontal.w,
             right: kPagePaddingHorizontal.w,
           ),
@@ -49,16 +75,18 @@ class PhoneView extends StatelessWidget {
               Form(
                 child: Column(
                   children: [
-                    LoginPageTextFormField(
+                    CustomTextFormField(
                       ctrlr: _emailCtrlr,
                       hint: 'Email',
                       isPassword: kFalse,
+                      keyboardType: TextInputType.emailAddress,
                     ),
                     SizedBox(height: 2.h),
-                    LoginPageTextFormField(
+                    CustomTextFormField(
                       ctrlr: _passwordCtrlr,
                       hint: 'Password',
                       isPassword: kTrue,
+                      keyboardType: TextInputType.visiblePassword,
                     ),
                   ],
                 ),
@@ -77,46 +105,37 @@ class PhoneView extends StatelessWidget {
                 ),
               ),
               SizedBox(height: 3.h),
-              CustomButton(
-                onPress: () async {
-                  authentication.signIn(
-                    _emailCtrlr.text,
-                    _passwordCtrlr.text,
-                  );
-                },
-                text: 'Sign In',
-                textColor: Get.theme.primaryColor,
-                bgColor: Get.isDarkMode
-                    ? kTextFormFieldColorDarkTheme
-                    : kTextFormFieldColorLightTheme,
-              ),
-              Row(
-                children: [
-                  Flexible(
-                    child: Text(
-                      'Don\'t have an account?',
-                      style: TextStyle(
-                        color: Get.isDarkMode
-                            ? kTextColorDarkTheme
-                            : kTextColorLightTheme,
-                      ),
+              isLoading
+                  ? SpinKitSpinningLines(
+                      color: Get.theme.primaryColor,
+                      lineWidth: 1,
+                      itemCount: 5,
+                      size: 50,
+                    )
+                  : CustomButton(
+                      onPress: () async {
+                        setState(() {
+                          isLoading = true;
+                        });
+                        authentication.signIn(
+                          _emailCtrlr.text,
+                          _passwordCtrlr.text,
+                        );
+                        Future.delayed(
+                          const Duration(seconds: 3),
+                          () => setState(
+                            () {
+                              isLoading = false;
+                            },
+                          ),
+                        );
+                      },
+                      text: 'Sign In',
+                      textColor: Get.theme.primaryColor,
+                      bgColor: Get.isDarkMode
+                          ? kTextFormFieldColorDarkTheme
+                          : kTextFormFieldColorLightTheme,
                     ),
-                  ),
-                  Align(
-                    alignment: Alignment.topRight,
-                    child: TextButton(
-                      onPressed: () {},
-                      child: Text(
-                        'Sign Up',
-                        style: TextStyle(
-                          fontSize: 10.sp,
-                          color: Get.theme.primaryColor,
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              )
             ],
           ),
         ),
