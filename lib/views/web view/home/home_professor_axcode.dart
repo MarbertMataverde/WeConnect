3879fbaa6ae6048390aa0fd49672f36eba4d1f4b
Mar Bert John Mataverde <marbertjohnmataverde@protonmail.com>
2022-02-11 23:36:30 +0800

    🚜Refactor - Validation

--- lib/views/web view/home/home_professor_axcode.dart
+++ lib/views/web view/home/home_professor_axcode.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:sizer/sizer.dart';
 import 'package:weconnect/constant/constant.dart';
@@ -58,6 +59,14 @@ class ProfessorAxCodeGenerator extends StatelessWidget {
                   ctrlr: _professorAxCodeCtrlr,
                   hint: 'Number of access code..',
                   isPassword: kFalse,
+                  inputFormater: <TextInputFormatter>[
+                    FilteringTextInputFormatter.allow(RegExp(r'[0-9]')),
+                  ],
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Enter The Number of Access Code For Professors👨🏻‍💻';
+                    }
+                  },
                 ),
               ),
               SizedBox(height: 3.h),
