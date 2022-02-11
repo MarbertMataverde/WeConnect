fc2810c19382fad7912668e45e28eb565a3507c3
Mar Bert John Mataverde <marbertjohnmataverde@protonmail.com>
2022-02-11 08:58:54 +0800

    🎨Refactor TextFormField UI

---
+++ lib/widgets/widget sign in/widget_textformfield_login.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../../constant/constant.dart';
+import '../../constant/constant_colors.dart';
+import '../../constant/constant_login_page.dart';
+
+class LoginPageTextFormField extends StatelessWidget {
+  const LoginPageTextFormField({
+    Key? key,
+    required this.ctrlr,
+    required this.hint,
+    required this.isPassword,
+  }) : super(key: key);
+  //?controller
+  final TextEditingController ctrlr;
+  //?hint
+  final String hint;
+  //?it is password or not
+  final bool isPassword;
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: ctrlr,
+      obscureText: isPassword,
+      style: kLoginPageTextFormFieldTextStyle,
+      cursorColor: Get.isDarkMode
+          ? kTextFormFieldCursorColorDarkTheme
+          : kTextFormFieldCursorColorLightTheme,
+      decoration: InputDecoration(
+        filled: kTrue,
+        fillColor: Get.isDarkMode
+            ? kTextFormFieldColorDarkTheme
+            : kTextFormFieldColorLightTheme,
+        hintText: hint,
+        border: kLoginPageTextFormFieldOutlineInputBorder,
+        focusedBorder: kLoginPageTextFormFieldFocusedBorder,
+      ),
+    );
+  }
+}
