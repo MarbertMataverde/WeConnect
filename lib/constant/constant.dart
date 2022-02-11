61b94ea24454cca469539963bb84001e328e5897
Mar Bert John Mataverde <marbertjohnmataverde@protonmail.com>
2022-02-11 23:33:05 +0800

    🗃Refactor Constant - Default Profile

--- lib/constant/constant.dart
+++ lib/constant/constant.dart
@@ -1,10 +1,11 @@
 //?phone padding
-import 'package:flutter_spinkit/flutter_spinkit.dart';
-import 'package:get/get.dart';
-
 const double kPagePaddingHorizontal = 6.0;
 const double kPagePaddingVertial = 5.0;
 
 //?constant bool
 const bool kTrue = true;
 const bool kFalse = false;
+
+//?default profile image
+const String kDefaultProfile =
+    'https://static.wikia.nocookie.net/itstabletoptime/images/b/b5/Default.jpg/revision/latest?cb=20210606184459';
