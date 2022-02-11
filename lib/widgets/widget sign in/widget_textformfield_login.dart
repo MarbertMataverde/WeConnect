4faa78f0345a16009fc41f08404cb7cd9675ffda
Mar Bert John Mataverde <marbertjohnmataverde@protonmail.com>
2022-02-11 15:42:24 +0800

    🚜⌨Refactor Class Name - Add Keyboardyype cntstrt.

--- lib/widgets/widget sign in/widget_textformfield_login.dart
+++ lib/widgets/widget sign in/widget_textformfield_login.dart
@@ -5,12 +5,13 @@ import '../../constant/constant.dart';
 import '../../constant/constant_colors.dart';
 import '../../constant/constant_login_page.dart';
 
-class LoginPageTextFormField extends StatelessWidget {
-  const LoginPageTextFormField({
+class CustomTextFormField extends StatelessWidget {
+  const CustomTextFormField({
     Key? key,
     required this.ctrlr,
     required this.hint,
     required this.isPassword,
+    this.keyboardType,
   }) : super(key: key);
   //?controller
   final TextEditingController ctrlr;
@@ -18,9 +19,11 @@ class LoginPageTextFormField extends StatelessWidget {
   final String hint;
   //?it is password or not
   final bool isPassword;
+  final TextInputType? keyboardType;
   @override
   Widget build(BuildContext context) {
     return TextFormField(
+      keyboardType: keyboardType,
       controller: ctrlr,
       obscureText: isPassword,
       style: kLoginPageTextFormFieldTextStyle,
