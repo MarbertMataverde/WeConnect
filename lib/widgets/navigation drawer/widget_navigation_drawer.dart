8efbdd021fd68bed3770da4b669103b42dc9802e
Mar Bert John Mataverde <marbertjohnmataverde@protonmail.com>
2022-03-25 00:55:14 +0800

    Revert "✨ Terms and Condition Page"
    This reverts commit b0d9605a186f8386d0ca7a9ae317c8db3c4e99a4.

--- lib/widgets/navigation drawer/widget_navigation_drawer.dart
+++ lib/widgets/navigation drawer/widget_navigation_drawer.dart
@@ -129,7 +129,14 @@ class WidgetNavigationDrawer extends StatelessWidget {
               title: 'Terms and Condition',
               icon: MdiIcons.fileOutline,
               onCliked: () {
-                selectedItem(context, 9);
+                selectedItem(context, 7);
+              },
+            ),
+            drawerItems(
+              title: 'Privacy Policy',
+              icon: MdiIcons.shieldAccountVariantOutline,
+              onCliked: () {
+                selectedItem(context, 8);
               },
             ),
             drawerItems(
