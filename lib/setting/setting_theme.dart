649f8204dc10c212228a94f8ac973cb0c3aebb14
Mar Bert John Mataverde <marbertjohnmataverde@protonmail.com>
2022-03-25 10:05:44 +0800

    ✨Font Improved

--- lib/setting/setting_theme.dart
+++ lib/setting/setting_theme.dart
@@ -4,12 +4,12 @@ final lightThemeData = ThemeData(
   brightness: Brightness.light,
   primaryColor: const Color(0xff05AAFA),
   scaffoldBackgroundColor: const Color(0xffffffff),
-  fontFamily: 'Nunito',
+  fontFamily: 'Inter',
 );
 
 final darkThemeData = ThemeData(
   brightness: Brightness.dark,
   primaryColor: const Color(0xff03E0CF),
   scaffoldBackgroundColor: const Color(0xff0B182F),
-  fontFamily: 'Nunito',
+  fontFamily: 'Inter',
 );
