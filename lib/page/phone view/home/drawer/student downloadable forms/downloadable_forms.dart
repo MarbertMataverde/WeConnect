b18fa8597052a1d45a912299782323ca3a63f621
Mar Bert John Mataverde <Marbertjohnmataverde@protonmail.com>
2022-04-13 16:47:25 +0800

    update ui forms

--- lib/page/phone view/home/drawer/student downloadable forms/downloadable_forms.dart
+++ lib/page/phone view/home/drawer/student downloadable forms/downloadable_forms.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+import 'package:iconsax/iconsax.dart';
 import 'package:sizer/sizer.dart';
 import 'package:url_launcher/url_launcher.dart';
-import '../../../../../constant/constant_colors.dart';
 import '../../../../../constant/constant_forms_url.dart';
 
-import '../../../../../widgets/appbar/appbar_title.dart';
+import '../../../../../widgets/appbar/build_appbar.dart';
 
 class DownloadForms extends StatelessWidget {
   const DownloadForms({Key? key}) : super(key: key);
@@ -14,11 +13,18 @@ class DownloadForms extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        backgroundColor: Colors.transparent,
-        title: const AppBarTitle(title: 'Forms'),
-        centerTitle: true,
+      appBar: buildAppBar(
+        context: context,
+        title: 'Forms',
+        leading: IconButton(
+          onPressed: () {
+            Get.back();
+          },
+          icon: Icon(
+            Iconsax.arrow_square_left,
+            color: Theme.of(context).iconTheme.color,
+          ),
+        ),
       ),
       body: Padding(
         padding: EdgeInsets.all(5.w),
@@ -27,65 +33,94 @@ class DownloadForms extends StatelessWidget {
             _buildTitle(aboutTitle: 'Application for Admission Forms'),
             const Divider(),
             _buildDOwnloadFormListTile(
-                title: 'Senior High School', url: kSeniorHighSchool),
+                context: context,
+                title: 'Senior High School',
+                url: kSeniorHighSchool),
             _buildDOwnloadFormListTile(
-                title: 'Undergraduate (College)', url: kUndergraduateCollege),
+                context: context,
+                title: 'Undergraduate (College)',
+                url: kUndergraduateCollege),
             _buildDOwnloadFormListTile(
-                title: 'Graduate School', url: kGraduateSchool),
+                context: context,
+                title: 'Graduate School',
+                url: kGraduateSchool),
             _buildDOwnloadFormListTile(
-                title: 'Foreign Students', url: kForeignStudents),
+                context: context,
+                title: 'Foreign Students',
+                url: kForeignStudents),
             //
             _buildTitle(aboutTitle: 'Free Tuition Fee Forms'),
             const Divider(),
             _buildDOwnloadFormListTile(
+                context: context,
                 title: 'Free Tuition Fee Pre-registration form',
                 url: kFreeTuitionFeePreRegistrationform),
             _buildDOwnloadFormListTile(
+                context: context,
                 title: 'Free Tuition Fee form (Registrar’s Copy)',
                 url: kFreeTuitionFeeformRegistrarsCopy),
             _buildDOwnloadFormListTile(
+                context: context,
                 title: 'Free Tuition Fee form (OSDS’s Copy)',
                 url: kFreeTuitionFeeformOSDSsCopy),
             //
             _buildTitle(aboutTitle: 'Graduate School Forms'),
             const Divider(),
             _buildDOwnloadFormListTile(
+                context: context,
                 title: 'Application for Registration',
                 url: kApplicationforRegistration),
             _buildDOwnloadFormListTile(
+                context: context,
                 title: 'Application for Proposal Defense',
                 url: kApplicationforProposalDefense),
             _buildDOwnloadFormListTile(
+                context: context,
                 title: 'Application for Final Oral Defense',
                 url: kApplicationforFinalOralDefense),
             //
             _buildTitle(aboutTitle: 'Other Forms'),
             const Divider(),
             _buildDOwnloadFormListTile(
-                title: 'Completion form', url: kCompletionform),
+                context: context,
+                title: 'Completion form',
+                url: kCompletionform),
             _buildDOwnloadFormListTile(
-                title: 'Dropping and Adding form', url: kDroppingandAddingform),
+                context: context,
+                title: 'Dropping and Adding form',
+                url: kDroppingandAddingform),
             _buildDOwnloadFormListTile(
-                title: 'Request and Claim Slip', url: kRequestandClaimSlip),
+                context: context,
+                title: 'Request and Claim Slip',
+                url: kRequestandClaimSlip),
             _buildDOwnloadFormListTile(
+                context: context,
                 title: 'Self Liquidating Program form (SLP)',
                 url: kSelfLiquidatingProgramformSLP),
             _buildDOwnloadFormListTile(
-                title: 'Shifting form', url: kShiftingform),
+                context: context, title: 'Shifting form', url: kShiftingform),
             _buildDOwnloadFormListTile(
-                title: 'Students Clearance', url: kStudentsClearance),
+                context: context,
+                title: 'Students Clearance',
+                url: kStudentsClearance),
             _buildDOwnloadFormListTile(
+                context: context,
                 title: 'Waiver for non-compliance of grades',
                 url: kWaiverForNonComplianceOfGrades),
             _buildDOwnloadFormListTile(
+                context: context,
                 title: 'Application for graduation form',
                 url: kApplicationforgraduationform),
             _buildDOwnloadFormListTile(
-                title: 'Medical Record Form', url: kMedicalRecordForm),
+                context: context,
+                title: 'Medical Record Form',
+                url: kMedicalRecordForm),
             _buildDOwnloadFormListTile(
+                context: context,
                 title: 'Individual Dental Health Record Form',
                 url: kIndividualDentalHealthRecordForm),
             _buildDOwnloadFormListTile(
+                context: context,
                 title: 'Student Information Sheet',
                 url: kStudentInformationSheet),
           ],
@@ -101,9 +136,8 @@ Widget _buildTitle({
   return Text(
     aboutTitle,
     textScaleFactor: 1.3,
-    style: TextStyle(
-      color: Get.isDarkMode ? kTextColorDarkTheme : kTextColorLightTheme,
-      fontWeight: FontWeight.bold,
+    style: const TextStyle(
+      fontWeight: FontWeight.w600,
     ),
   );
 }
@@ -111,19 +145,21 @@ Widget _buildTitle({
 Widget _buildDOwnloadFormListTile({
   required String title,
   required String url,
+  required BuildContext context,
 }) {
   return ListTile(
     title: Text(
       title,
       style: TextStyle(
-        color: Get.isDarkMode ? kTextColorDarkTheme : kTextColorLightTheme,
+        color: Theme.of(context).textTheme.labelMedium!.color,
       ),
     ),
     trailing: IconButton(
+      splashRadius: 3.h,
       onPressed: () => openBroweserUrl(url: url),
       icon: Icon(
-        MdiIcons.fileDownloadOutline,
-        color: Get.theme.primaryColor,
+        Iconsax.document_download,
+        color: Theme.of(context).primaryColor,
       ),
     ),
   );
