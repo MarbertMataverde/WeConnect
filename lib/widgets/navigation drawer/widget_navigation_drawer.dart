38d20ccc3b7a0f8cf4f409b013224c830cf2be06
Mar Bert John Mataverde <Marbertjohnmataverde@protonmail.com>
2022-04-14 17:05:15 +0800

    remove unused theme changer

--- lib/widgets/navigation drawer/widget_navigation_drawer.dart
+++ lib/widgets/navigation drawer/widget_navigation_drawer.dart
@@ -1,17 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:iconsax/iconsax.dart';
 import 'package:sizer/sizer.dart';
 import 'package:weconnect/setting/them_config.dart';
 import '../../controller/controller_account_information.dart';
 
-import '../../controller/controller_theme_changer.dart';
 import 'drawer_items.dart';
 import 'named_divider.dart';
 import 'selected_item_function.dart';
-
-final changeTheme = Get.put(ControllerChangeTheme());
 
 //accessing global box
 final box = GetStorage();
