2220054caae680ff9b315ebb7968cdbd4efd7ff3
Mar Bert John Mataverde <Marbertjohnmataverde@protonmail.com>
2022-04-14 12:07:41 +0800

    fix auto appbar auto leading

--- lib/widgets/appbar/build_appbar.dart
+++ lib/widgets/appbar/build_appbar.dart
@@ -5,8 +5,10 @@ buildAppBar({
   required String title,
   List<Widget>? actions,
   Widget? leading,
+  bool? autoLeading,
 }) {
   return AppBar(
+    automaticallyImplyLeading: autoLeading ?? false,
     backgroundColor: Colors.transparent,
     elevation: 0.0,
     leading: leading,
