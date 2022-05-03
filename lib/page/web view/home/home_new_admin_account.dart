9c409ba4a2dafb62f3a92e8ccacebf4e0f34332d
Mar Bert John Mataverde <Marbertjohnmataverde@protonmail.com>
2022-05-03 13:49:28 +0800

    new admin account step 1 menu and routing

---
+++ lib/page/web view/home/home_new_admin_account.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class NewAdminAccount extends StatelessWidget {
+  const NewAdminAccount({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          child: Text('Hello World'),
+        ),
+      ),
+    );
+  }
+}
--- lib/page/web view/home/home_web_wrapper.dart
+++ lib/page/web view/home/home_web_wrapper.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:focused_menu/focused_menu.dart';
 import 'package:get/get.dart';
 import 'package:iconsax/iconsax.dart';
+import 'package:sizer/sizer.dart';
+import 'package:weconnect/page/web%20view/home/home_new_admin_account.dart';
 
 import '../../../authentication/authentication_controller.dart';
+import '../../../controller/controller_post_tile_pop_up_menu.dart';
 import 'home_student_axcode.dart';
 
 final authentication = Get.put(Authentication());
@@ -22,27 +26,51 @@ class _HomeWrapperState extends State<HomeWebWrapper> {
         elevation: 0,
         backgroundColor: Colors.transparent,
         centerTitle: true,
-        title: Text(
-          'WeConnect',
-          textScaleFactor: 1.5,
-          style: TextStyle(
-            color: Theme.of(context).primaryColor,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
         actions: [
-          IconButton(
-            onPressed: () {
-              authentication.signOut();
-            },
-            icon: Icon(
-              Iconsax.logout,
-              color: Theme.of(context).iconTheme.color,
-            ),
-          )
+          meneHolder(context),
         ],
       ),
       body: const StudentAxCodeGenerator(),
     );
   }
+}
+
+FocusedMenuHolder meneHolder(BuildContext context) {
+  return FocusedMenuHolder(
+    menuWidth: MediaQuery.of(context).size.width * 0.30,
+    blurSize: 1.0,
+    menuBoxDecoration: BoxDecoration(
+        color: Colors.transparent,
+        borderRadius: BorderRadius.all(Radius.circular(1.w))),
+    duration: const Duration(milliseconds: 100),
+    animateMenuItems: false,
+    blurBackgroundColor: Colors.black,
+    openWithTap: true,
+    onPressed: () {},
+    menuItems: [
+      focusMenuItem(
+        'New Admin Account',
+        Iconsax.flash,
+        Colors.black54,
+        () {
+          Get.to(() => const NewAdminAccount());
+        },
+      ),
+      focusMenuItem(
+        'Sign Out',
+        Iconsax.logout,
+        Colors.black54,
+        () {
+          authentication.signOut();
+        },
+      ),
+    ],
+    child: Padding(
+      padding: EdgeInsets.only(right: 2.w),
+      child: Icon(
+        Iconsax.menu,
+        color: Theme.of(context).iconTheme.color,
+      ),
+    ),
+  );
 }
