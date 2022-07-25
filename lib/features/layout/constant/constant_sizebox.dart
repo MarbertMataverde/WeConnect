f2b39036c7e7fce42a6ab2a0b0079a861d598002
Mar Bert John Mataverde <marbertmataverde.dev@gmail.com>
2022-07-25 15:39:16 +0800

    added constant sizebox
    This is just a sizebox with the default value of 10

--- lib/features/layout/constant/constant_sizebox.dart
+++ lib/features/layout/constant/constant_sizebox.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/widgets.dart';
 
+/// This is a global SizeBox that has a default value of 10
 SizedBox sizedBox({double? height}) {
   return SizedBox(
     height: height ?? 10,
